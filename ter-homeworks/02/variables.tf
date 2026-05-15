573377920c82e5066c16142b05a22b12bdc87a53
Dmitry Letunov <letunov_2003@mail.ru>
2026-05-15 08:07:50 +0000

    Fix syntax errors, add Terraform config for task 02

---
+++ ter-homeworks/02/variables.tf
@@ -0,0 +1,30 @@
+variable "cloud_id" {
+  type        = string
+  description = "Cloud ID"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Folder ID"
+}
+
+variable "default_zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "default_cidr" {
+  type    = list(string)
+  default = ["10.0.1.0/24"]
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "develop"
+}
+
+variable "vms_ssh_public_root_key" {
+  type        = string
+  default     = ""
+  description = "Public SSH key for VM"
+}
