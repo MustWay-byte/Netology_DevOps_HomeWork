dd3d6eb6f9f3181a6b471aa0709b7551602ed518
Dmitry Letunov <letunov_2003@mail.ru>
2026-05-22 08:27:08 +0000

    feat: migrate state to S3 backend with native locking

--- ter-homeworks/04/providers.tf
+++ ter-homeworks/04/providers.tf
@@ -1,10 +1,31 @@
 terraform {
-  required_providers {
-    yandex = { source = "yandex-cloud/yandex", version = "0.203.0" }
-    random = { source = "hashicorp/random", version = "3.5.0" }
+  required_version = "~>1.15.0"
+  
+  backend "s3" {
+    bucket  = "simple-bucket-mustway-abc123"
+    key     = "terraform.tfstate"
+    region  = "ru-central1"
+    
+    use_lockfile = true
+    
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
-  required_version = ">= 1.8.4"
+  
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.203"
+    }
+  }
 }
+
 provider "yandex" {
   cloud_id                 = "b1gr8dpt0el2sc1i0p33"
   folder_id                = "b1g5m6nu8f7nl3dmsjot"
